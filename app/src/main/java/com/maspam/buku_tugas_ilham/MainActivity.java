609e04925e4b8bb169a3f5082c246e2b00ab55fb
Lemenn <mrlmens01001@gmail.com>
2023-04-11 00:58:21 +0700

    Initial Commmit

---
+++ app/src/main/java/com/maspam/buku_tugas_ilham/MainActivity.java
@@ -0,0 +1,14 @@
+package com.maspam.buku_tugas_ilham;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.latihan_layout);
+    }
+}
